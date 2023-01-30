af859c17d4cbc1c1a07d4b34b759031530069f8b
aberizuki <zalaarthurn@gmail.com>
2023-01-30 07:50:56 +0700

    axios

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Outlet, Link } from "react-router-dom";
 import Header from "../../components/header";
 import { useDispatch, useSelector } from "react-redux";
